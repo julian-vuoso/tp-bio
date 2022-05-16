f2967f1f2013707ce870f5f80399355c1992106a
Nicolas Britos <nicbritos@hotmail.com>
2022-05-16 20:45:01 -0300

    ej1

--- Ej1.py
+++ Ej1.py
@@ -8,7 +8,7 @@ from Bio import SeqIO
 def save_sequence(seq, i):
     seq.id = 'e1'
     seq.description = 'ORF' + str(i)
-    SeqIO.write(seq, "out1/%s-%d.fasta" % ('out', i), 'fasta')
+    SeqIO.write(seq, 'out1/out-' + str(i) + '.fasta', 'fasta')
 
 
 def parse_genbank(filename):
@@ -17,10 +17,10 @@ def parse_genbank(filename):
 
 def ej1(data, min_len=300):
     i = 1
-    for strand, nuc in [(1, data.seq), (-1, data.seq.reverse_complement())]:
+    for _, nuc in [(1, data.seq), (-1, data.seq.reverse_complement())]:
         for frame in range(3):
-            length = math.floor(3 * ((len(data) - frame) // 3))
-            for protein in nuc[frame:frame+length].translate(1).split("*"):
+            length = math.floor(3 * ((len(data) - frame) / 3))
+            for protein in nuc[frame:frame + length].translate(1).split("*"):
                 if len(protein) >= min_len:
                     save_sequence(SeqRecord(protein), i)
                     i += 1
